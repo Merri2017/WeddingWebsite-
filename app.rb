7f4db1898db351ccbfb2ffd7995d34e598a0224a
merri2017 <merrishel.richardson@gmail.com>
2018-04-15 17:33:13 -0400

    page is rendering to screen with css and javascript

--- app.rb
+++ app.rb
@@ -2,18 +2,28 @@ require 'sinatra'
 # contains all the routing code 
 
 get '/' do
+	erb :homepage
 
 end
 
 get '/contact' do
+	erb :contact
 
 end
 
 
 get '/staff' do 
+	erb :staff
 
 end
 
-get '/staff/:id' do 
+get '/staff/:name' do 
+    if params[:name] == "urara"
+        erb :urara
+    elsif params[:name] == "boton"
+        erb :boton
+    else params[:name] == "kobato"
+    	erb :kobato
+end
 
 end
